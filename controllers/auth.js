8840b8c7d7f6d9b411b735d234c4b4617f07b301
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-25 20:05:04 +0530

    Adding the check, when all the fileds are not filled by the person

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 const User = require('../models/user');
 
 const register = async (req, res) => {
   try {
-    res.status(200).json({ message: '/auth/register route' });
+    const { name, email, password } = req.body;
+
+    // Checking if all the required fields are provided, and throwing an error if not
+    if (!name || !email || !password) {
+      throw new BadRequestError('Please provide all the required fields');
+    }
+
+    // Creating a user in the MongoDB database
+    const user = await User.create({ ...req.body });
+
+    // Sending a response to the client
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: '/auth/register route', user });
   } catch (error) {
     console.log(error.message);
     res.status(404).json({ message: error.message });
@@ -11,7 +26,7 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    res.status(200).json({ message: '/auth/login route' });
+    res.status(200).json({ message: '/auth/login route', data: req.body });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
