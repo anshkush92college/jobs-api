60a13ef1a44ff5fdb43e5d2fa4ec23510bf77063
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-25 17:05:37 +0530

    Created the user model for the app

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    // Checking for the valid email format
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
