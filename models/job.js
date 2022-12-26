4918c17b4dc7e3ac5734a184ed27cf41479ea07c
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-26 12:38:20 +0530

    Created the job schema for the Jobs that we are going to stroe in the MONGO DB database

---
+++ models/job.js
@@ -0,0 +1,39 @@
+const { model, Schema, mongoose } = require('mongoose');
+
+const User = require('./user');
+
+const jobSchema = new Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: [
+        'interview',
+        'declined',
+        'pending',
+        'offer',
+        'accepted',
+        'rejected',
+      ],
+      default: 'pending',
+    },
+    createdBy: {
+      // Using the User model id as a value to the createdBy field
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Job = model('Job', jobSchema);
+
+module.exports = Job;
